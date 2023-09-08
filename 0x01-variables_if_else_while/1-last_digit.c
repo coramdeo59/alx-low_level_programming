bd9fbb8e3fa8ccc6e3cf8a33dcb255ac36852a32
coramdeo59 <melkamuelias59@gmail.com>
2023-09-08 14:40:40 -0700

    add first task

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,7 +19,7 @@ int main(void)
 		printf("%d and is 0\n", n);
 	else if (n > 5)
 		printf("%d and is greater than 5\n", n);
-	else if (n < 6 & != 0)
+	else if (n < 6 && != 0)
 		printf("%d and is less than 6 and not 0",n);
 
 	return (0);
